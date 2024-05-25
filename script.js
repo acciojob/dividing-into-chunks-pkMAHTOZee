401beca13eef01b97c21526dfc2cfe0273d41820
pkMAHTOZee <86905216+pkMAHTOZee@users.noreply.github.com>
2024-05-25 07:45:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
-
 const divide = (arr, n) => {
-  // Write your code here
+  let result = [];
+  let currentSubarray = [];
+  let currentSum = 0;
+
+  for (let num of arr) {
+    if (currentSum + num <= n) {
+      currentSubarray.push(num);
+      currentSum += num;
+    } else {
+      result.push(currentSubarray);
+      currentSubarray = [num];
+      currentSum = num;
+    }
+  }
+
+  if (currentSubarray.length > 0) {
+    result.push(currentSubarray);
+  }
+
+  return result;
 };
 
-const n = prompt("Enter n: ");
+const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+const n = parseInt(prompt("Enter n: "), 10);
 alert(JSON.stringify(divide(arr, n)));
